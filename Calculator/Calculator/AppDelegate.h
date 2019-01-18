b6d214b72d693775de54b9e1db815bf96f784cc3
annieli <annieli@kkbox.com>
2019-01-18 11:55:06 +0800

    Create project.

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Annie Lee on 2019/1/18.
+//  Copyright © 2019 Annie Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
